b083d2b2b00e1339ccbcc24fa0bf4520c2f8607e
Octavia Cephas <octavia.cephas@gmail.com>
2018-03-04 18:16:45 -0500

    htmlRoutes

--- FriendFinder/app/data/friends.js
+++ FriendFinder/app/data/friends.js
@@ -1,24 +1,35 @@
-const http = require("http");
+const express = require("express");
 
 const bodyParser = require("body-parser");
 
-// Set our port to 3000
+const path = require("path");
+
+// Set up Express app
+const app = express();
 const PORT = 3000;
 
-const server = http.createServer(handleRequest);
-
-function handleRequest(req, res) {
-
-  // Capture the url the request is made to
- const path = req.url;
-
-  // Express app to handle data parsing
-	app.use(bodyParser.urlencoded({ extended: false }));
-	app.use(bodyParser.json());
+// Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 
 
+// const server = http.createServer(handleRequest);
+
+// function handleRequest(req, res) {
+
+//   // Capture the url the request is made to
+//  const path = req.url;
+// };
+  
+// Directs server to a series of "route" files.
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+
+// Starts the server to begin listening
 
   server.listen(PORT, function() {
   console.log("Server is listening on PORT: " + PORT);
+
 });
